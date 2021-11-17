c06be7430a583b20921881eec872beb71fb7a9de
Adiel Seffrin <adielseffrin@gmail.com>
2021-11-17 08:31:04 -0300

    new localhost port and url

--- app.php
+++ app.php
@@ -5,8 +5,8 @@ use Ratchet\ConnectionInterface;
 use MyApp\Socket;
 
 require dirname( __FILE__ ) . '/vendor/autoload.php';
-
-$app = new Ratchet\App("localhost", 8091, '0.0.0.0', $loop);
+$httpHost = "ws.adielseffr.in";
+$app = new Ratchet\App("ws://$httpHost", 8080, '0.0.0.0', $loop);
 $app->route('/chat', new Socket, array('*'));
 
 $app->run();

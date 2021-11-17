9ab56db5a9d7dcb17a579f05653d5f2440638bbf
Adiel Seffrin <adielseffrin@gmail.com>
2021-11-17 08:13:10 -0300

    fix host of socket

--- app.php
+++ app.php
@@ -6,7 +6,7 @@ use MyApp\Socket;
 
 require dirname( __FILE__ ) . '/vendor/autoload.php';
 
-$app = new Ratchet\App("https://ws.adielseffr.in", 80, '0.0.0.0', $loop);
+$app = new Ratchet\App("localhost", 8080, '0.0.0.0', $loop);
 $app->route('/chat', new Socket, array('*'));
 
 $app->run();

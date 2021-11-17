b09e2bbadbc52a31e9584d91433455aafc7c7b87
Adiel Seffrin <adielseffrin@gmail.com>
2021-11-17 08:33:37 -0300

    new localhost url

--- app.php
+++ app.php
@@ -6,7 +6,7 @@ use MyApp\Socket;
 
 require dirname( __FILE__ ) . '/vendor/autoload.php';
 $httpHost = "ws.adielseffr.in";
-$app = new Ratchet\App("ws://$httpHost", 8080, '0.0.0.0', $loop);
+$app = new Ratchet\App($httpHost, 8080, '0.0.0.0', $loop);
 $app->route('/chat', new Socket, array('*'));
 
 $app->run();

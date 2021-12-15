172d3f413edee644744310ea949e307f29a718a7
Adiel Seffrin <adielseffrin@gmail.com>
2021-12-15 08:11:45 -0300

    change endpoint

--- app.php
+++ app.php
@@ -7,6 +7,6 @@ use MyApp\Socket;
 require dirname( __FILE__ ) . '/vendor/autoload.php';
 $httpHost = "ws.adielseffr.in";
 $app = new Ratchet\App($httpHost, 8080, '0.0.0.0', $loop);
-$app->route('/chat', new Socket, array('*'));
+$app->route('/pizza', new Socket, array('*'));
 
 $app->run();
